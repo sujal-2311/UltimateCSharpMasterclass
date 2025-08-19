c496de55d8593e3062bc4068fb0d3fda40cd6e91
Sujal <sujalamirani@gmail.com>
2025-08-19 23:57:07 +0530

    initial commit

---
+++ TodoList/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you Want to do?");
+Console.WriteLine("[s]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+int number;
+number = 7;
+Console.WriteLine(number);
+
+int a = 1, b = 6;
+string name, lastname;
+
+Console.ReadKey();
